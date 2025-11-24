ffee6db0da465d2cfdfa5c69100b8df729df150e
Shidqi <shidqi@Shidqis-MacBook-Pro.local>
2025-11-24 19:21:28 +0700

    fix(methodHome): init swiper with min width 887px

--- src/animations/methodHome.js
+++ src/animations/methodHome.js
@@ -5,179 +5,192 @@ import { Rive, Layout, Fit, Alignment } from "@rive-app/webgl2";
 gsap.registerPlugin(ScrollTrigger);
 
 export function initMethodHomeAnimation() {
-	const section = document.querySelector(".method_home_wrap");
-	const rSource = document.querySelector("#homeRiveSrc");
-	let swiperInstance = null;
+    const section = document.querySelector(".method_home_wrap");
+    const rSource = document.querySelector("#homeRiveSrc");
+    let swiperInstance = null;
 
-	if (!section) return;
+    let mm = gsap.matchMedia();
 
-	function initSwiper() {
-		if (swiperInstance) {
-			swiperInstance.destroy(true, true);
-			swiperInstance = null;
-		}
+    if (!section) return;
 
-		swiperInstance = new Swiper(".swiper.is-method", {
-			effect: "coverflow",
-			direction: "horizontal",
-			centeredSlides: true,
-			initialSlide: 1,
-			slidesPerView: "auto",
-			spaceBetween: 48,
-			allowTouchMove: true,
-			speed: 1000,
-			loop: true,
-			mousewheel: {
-				forceToAxis: true,
-				enabled: true,
-				sensitivity: 0.25,
-			},
-			// navigation: {
-			// 	nextEl: ".next",
-			// 	prevEl: ".prev",
-			// },
-			coverflowEffect: {
-				rotate: 0,
-				stretch: 0,
-				depth: 0,
-				modifier: 1,
-				slideShadows: false,
-			},
-		});
-	}
+    function initSwiper() {
+        if (swiperInstance) {
+            swiperInstance.destroy(true, true);
+            swiperInstance = null;
+        }
 
-	initSwiper();
+        swiperInstance = new Swiper(".swiper.is-method", {
+            effect: "coverflow",
+            direction: "horizontal",
+            centeredSlides: true,
+            initialSlide: 1,
+            slidesPerView: "auto",
+            spaceBetween: 48,
+            allowTouchMove: true,
+            speed: 1000,
+            loop: true,
+            mousewheel: {
+                forceToAxis: true,
+                enabled: true,
+                sensitivity: 0.25,
+            },
+            // navigation: {
+            // 	nextEl: ".next",
+            // 	prevEl: ".prev",
+            // },
+            coverflowEffect: {
+                rotate: 0,
+                stretch: 0,
+                depth: 0,
+                modifier: 1,
+                slideShadows: false,
+            },
+        });
+    }
 
-	// gsap.context(() => {
-	// 	let mm = gsap.matchMedia();
+    mm.add("(min-width: 887px)", () => {
+        initSwiper();
 
-	// 	mm.add("(min-width: 280px)", () => {
-	// 		const riveUrl = rSource?.dataset?.riveUrl;
-	// 		const stateMachine =
-	// 			rSource?.dataset?.riveStateMachine || "State Machine 1";
+        return () => {
+            if (swiperInstance) {
+                swiperInstance.destroy(true, true);
+                swiperInstance = null;
+            }
+        };
+    });
 
-	// 		if (!riveUrl) {
-	// 			console.error(
-	// 				"Missing Rive URL in #homeRiveSrc dataset (riveUrl)."
-	// 			);
-	// 			return;
-	// 		}
+    // initSwiper();
 
-	// 		const canvases = [
-	// 			{
-	// 				el: document.querySelector("#card_01"),
-	// 				artboard: "card_01",
-	// 			},
-	// 			{
-	// 				el: document.querySelector("#card_02"),
-	// 				artboard: "card_02",
-	// 			},
-	// 			{
-	// 				el: document.querySelector("#card_03"),
-	// 				artboard: "card_03",
-	// 			},
-	// 		];
+    // gsap.context(() => {
+    // 	let mm = gsap.matchMedia();
 
-	// 		const sm = stateMachine || undefined;
+    // 	mm.add("(min-width: 280px)", () => {
+    // 		const riveUrl = rSource?.dataset?.riveUrl;
+    // 		const stateMachine =
+    // 			rSource?.dataset?.riveStateMachine || "State Machine 1";
 
-	// 		const riveInstances = [];
+    // 		if (!riveUrl) {
+    // 			console.error(
+    // 				"Missing Rive URL in #homeRiveSrc dataset (riveUrl)."
+    // 			);
+    // 			return;
+    // 		}
 
-	// 		canvases.forEach(({ el, artboard }) => {
-	// 			if (!el) return;
+    // 		const canvases = [
+    // 			{
+    // 				el: document.querySelector("#card_01"),
+    // 				artboard: "card_01",
+    // 			},
+    // 			{
+    // 				el: document.querySelector("#card_02"),
+    // 				artboard: "card_02",
+    // 			},
+    // 			{
+    // 				el: document.querySelector("#card_03"),
+    // 				artboard: "card_03",
+    // 			},
+    // 		];
 
-	// 			try {
-	// 				const instance = new Rive({
-	// 					src: riveUrl,
-	// 					canvas: el,
-	// 					stateMachines: sm,
-	// 					artboard,
-	// 					autoplay: false,
-	// 					layout: new Layout({
-	// 						fit: Fit.Contain,
-	// 						alignment: Alignment.Center,
-	// 					}),
-	// 					isTouchScrollEnabled: true,
-	// 					onLoad: () => {
-	// 						try {
-	// 							instance.resizeDrawingSurfaceToCanvas();
-	// 						} catch (e) {
-	// 							// ignore resize errors if API not available
-	// 						}
+    // 		const sm = stateMachine || undefined;
 
-	// 						if (sm) {
-	// 							try {
-	// 								const inputs =
-	// 									instance.stateMachineInputs(sm);
-	// 								const playTrigger =
-	// 									inputs &&
-	// 									inputs.find((i) => i.name === "play");
-	// 								if (
-	// 									playTrigger &&
-	// 									typeof playTrigger.fire === "function"
-	// 								) {
-	// 									playTrigger.fire();
-	// 								}
-	// 							} catch (e) {
-	// 								// ignore state machine input errors
-	// 							}
-	// 						}
-	// 					},
-	// 					onLoadError: (err) => {
-	// 						console.error("Rive loading error:", err);
-	// 					},
-	// 				});
+    // 		const riveInstances = [];
 
-	// 				riveInstances.push(instance);
+    // 		canvases.forEach(({ el, artboard }) => {
+    // 			if (!el) return;
 
-	// 				const handlePlay = () => {
-	// 					instance.play();
-	// 				};
+    // 			try {
+    // 				const instance = new Rive({
+    // 					src: riveUrl,
+    // 					canvas: el,
+    // 					stateMachines: sm,
+    // 					artboard,
+    // 					autoplay: false,
+    // 					layout: new Layout({
+    // 						fit: Fit.Contain,
+    // 						alignment: Alignment.Center,
+    // 					}),
+    // 					isTouchScrollEnabled: true,
+    // 					onLoad: () => {
+    // 						try {
+    // 							instance.resizeDrawingSurfaceToCanvas();
+    // 						} catch (e) {
+    // 							// ignore resize errors if API not available
+    // 						}
 
-	// 				const handlePause = () => {
-	// 					instance.pause();
-	// 				};
+    // 						if (sm) {
+    // 							try {
+    // 								const inputs =
+    // 									instance.stateMachineInputs(sm);
+    // 								const playTrigger =
+    // 									inputs &&
+    // 									inputs.find((i) => i.name === "play");
+    // 								if (
+    // 									playTrigger &&
+    // 									typeof playTrigger.fire === "function"
+    // 								) {
+    // 									playTrigger.fire();
+    // 								}
+    // 							} catch (e) {
+    // 								// ignore state machine input errors
+    // 							}
+    // 						}
+    // 					},
+    // 					onLoadError: (err) => {
+    // 						console.error("Rive loading error:", err);
+    // 					},
+    // 				});
 
-	// 				ScrollTrigger.create({
-	// 					trigger: el,
-	// 					start: "top bottom",
-	// 					end: "bottom top",
-	// 					onEnter: handlePlay,
-	// 					onLeave: handlePause,
-	// 					onEnterBack: handlePlay,
-	// 					onLeaveBack: handlePause,
-	// 				});
-	// 			} catch (err) {
-	// 				console.error("Failed to create Rive instance:", err);
-	// 			}
-	// 		});
+    // 				riveInstances.push(instance);
 
-	// 		// cleanup when this media query is torn down
-	// 		return () => {
-	// 			riveInstances.forEach((inst) => {
-	// 				try {
-	// 					// try common cleanup methods if present
-	// 					if (typeof inst.destroy === "function") inst.destroy();
-	// 					else if (typeof inst.cleanup === "function")
-	// 						inst.cleanup();
-	// 					else if (typeof inst.stop === "function") inst.stop();
-	// 					// null reference for GC
-	// 					inst = null;
-	// 				} catch (e) {
-	// 					// swallow cleanup errors
-	// 				}
-	// 			});
-	// 		};
-	// 	});
+    // 				const handlePlay = () => {
+    // 					instance.play();
+    // 				};
 
-	// 	mm.add("(min-width: 992px)", () => {
-	// 		initSwiper();
+    // 				const handlePause = () => {
+    // 					instance.pause();
+    // 				};
 
-	// 		return () => {
-	// 			if (swiperInstance) {
-	// 				swiperInstance.destroy(true, true);
-	// 				swiperInstance = null;
-	// 			}
-	// 		};
-	// 	});
-	// }, section);
+    // 				ScrollTrigger.create({
+    // 					trigger: el,
+    // 					start: "top bottom",
+    // 					end: "bottom top",
+    // 					onEnter: handlePlay,
+    // 					onLeave: handlePause,
+    // 					onEnterBack: handlePlay,
+    // 					onLeaveBack: handlePause,
+    // 				});
+    // 			} catch (err) {
+    // 				console.error("Failed to create Rive instance:", err);
+    // 			}
+    // 		});
+
+    // 		// cleanup when this media query is torn down
+    // 		return () => {
+    // 			riveInstances.forEach((inst) => {
+    // 				try {
+    // 					// try common cleanup methods if present
+    // 					if (typeof inst.destroy === "function") inst.destroy();
+    // 					else if (typeof inst.cleanup === "function")
+    // 						inst.cleanup();
+    // 					else if (typeof inst.stop === "function") inst.stop();
+    // 					// null reference for GC
+    // 					inst = null;
+    // 				} catch (e) {
+    // 					// swallow cleanup errors
+    // 				}
+    // 			});
+    // 		};
+    // 	});
+
+    // 	mm.add("(min-width: 992px)", () => {
+    // 		initSwiper();
+
+    // 		return () => {
+    // 			if (swiperInstance) {
+    // 				swiperInstance.destroy(true, true);
+    // 				swiperInstance = null;
+    // 			}
+    // 		};
+    // 	});
+    // }, section);
 }
